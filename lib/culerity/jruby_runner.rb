dbef7008814a883700210a4d1405fe15364c9eaa
Matt Fletcher <fletcher@atomicobject.com>
2010-02-02 16:27:27 -0500

    bump jruby heap size to 2gb

--- lib/culerity/jruby_runner.rb
+++ lib/culerity/jruby_runner.rb
@@ -3,7 +3,7 @@ module Culerity
     def self.cmd
       jruby_complete = "#{File.dirname(__FILE__)}/../../vendor/jruby/jruby-complete-1.4.0.jar"
       celerity = "#{File.dirname(__FILE__)}/../../vendor/gems/celerity-0.7.6/lib"
-      "java -Xmx1024m -Xss1024k -jar #{jruby_complete} -I#{celerity}"
+      "java -Xmx2048m -Xss1024k -jar #{jruby_complete} -I#{celerity}"
     end
 
     def self.run(command_to_run)
