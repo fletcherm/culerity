ece412b2b34209f76433e0d5508886bb7850a4d4
Matt Fletcher <fletcher@atomicobject.com>
2010-04-30 17:29:36 -0400

    Remove RUBYOPTS from the JRuby process's environment. This avoids bundler 0.9+'s RUBYOPTS from disrupting the JRuby process's LOAD_PATH.

--- lib/culerity/jruby_runner.rb
+++ lib/culerity/jruby_runner.rb
@@ -3,7 +3,7 @@ module Culerity
     def self.cmd
       jruby_complete = "#{File.dirname(__FILE__)}/../../vendor/jruby/jruby-complete-1.4.0.jar"
       celerity = "#{File.dirname(__FILE__)}/../../vendor/gems/celerity-0.7.6/lib"
-      "java -Xmx512m -Xss1024k -jar #{jruby_complete} -I#{celerity}"
+      "RUBYOPTS='' java -Xmx512m -Xss1024k -jar #{jruby_complete} -I#{celerity}"
     end
 
     def self.run(command_to_run)
--- spec/jruby_runner_spec.rb
+++ spec/jruby_runner_spec.rb
@@ -1,6 +1,14 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Culerity::JRubyRunner do
+  before do
+    @original_rubyopts = ENV["RUBYOPTS"]
+  end
+
+  after do
+    ENV["RUBYOPTS"] = @original_rubyopts
+  end
+
   it "has the command for running jruby within jruby-complete" do
     Culerity::JRubyRunner.cmd.should match(/jruby/)
   end
@@ -8,5 +16,10 @@ describe Culerity::JRubyRunner do
   it "runs JRuby with the given parameters" do
     Culerity::JRubyRunner.run("--version").should match(/^jruby 1.4.0/)
     Culerity::JRubyRunner.run("-e 'puts 1'").chomp.should == "1"
+  end
+
+  it "ignores RUBYOPTS when running the JRuby process" do
+    ENV["RUBYOPTS"] = "some optz"
+    Culerity::JRubyRunner.run(%+-e 'puts ENV["RUBYOPTS"]'+).chomp.should == ""
   end
 end
