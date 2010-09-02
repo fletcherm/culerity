288eeff6a855b6fb42aa86df0e25947ca2209aa4
Matt Fletcher <fletcher@atomicobject.com>
2010-09-02 17:03:08 -0400

    put culerity into JRuby's load path

--- lib/culerity/jruby_runner.rb
+++ lib/culerity/jruby_runner.rb
@@ -3,7 +3,7 @@ module Culerity
     def self.cmd
       jruby_complete = "#{File.dirname(__FILE__)}/../../vendor/jruby/jruby-complete-1.4.0.jar"
       celerity = "#{File.dirname(__FILE__)}/../../vendor/gems/celerity-0.8.0.beta.4/lib"
-      "RUBYOPT='' java -Xmx512m -Xss1024k -jar #{jruby_complete} -I#{celerity}"
+      "RUBYOPT='' java -Xmx512m -Xss1024k -jar #{jruby_complete} -I#{celerity} -I#{Culerity.culerity_root}/lib"
     end
 
     def self.run(command_to_run)
